ce084476df9a8b7e3e0918e53989f7213aeb427d
Maciej Kędziora <44085390+gryzmol98@users.noreply.github.com>
2020-09-21 22:10:39 +0200

    litlle change to scroll fade

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ typewriter
 
 let video = document.querySelector('video');
 window.addEventListener('scroll', function(){
-    let value = 1 + window.scrollY/ -600;
+    let value = 1 + window.scrollY/ -400;
     video.style.opacity = value;
 })
