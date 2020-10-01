1e295da1a9d9978914b678ed09ffefceaf7fbcf5
Bartek <popiolekbartlomiej368@gmail.com>
2020-10-01 19:17:44 +0200

    fix hamburger menu

--- hamburger.js
+++ hamburger.js
@@ -1,9 +1,16 @@
 const burger = document.getElementById("burger");
 const nav = document.querySelector(".mobile");
+const body = document.querySelector("body");
 
 function handleClick(e) {
   this.classList.toggle("active");
   nav.classList.toggle("show");
+
+  if (this.classList.contains("active")) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "auto";
+  }
 }
 
 burger.addEventListener("click", handleClick);
