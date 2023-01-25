6f9c7ea0dc6a9f41defa807bc72b572d4632c1fb
Maciej Kędziora <maciejkedziora98@gmail.com>
2023-01-26 00:20:22 +0100

    update build.sh and app.js

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t maclucky/landing-page .
-docker tag maclucky/landing-page:latest maclucky/landing-page:1.0.1
-docker push maclucky/landing-page:1.0.1
+docker tag maclucky/landing-page:latest maclucky/landing-page:1.0.2
+docker push maclucky/landing-page:1.0.2
