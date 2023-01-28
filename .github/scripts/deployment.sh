0957e9c5a43ccf74b9a624941015619e5e3ebd7a
Maciej Kędziora <maciejkedziora98@gmail.com>
2023-01-28 23:34:15 +0100

    update deployment.sh

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -2,4 +2,4 @@
 # assign the first argument passed to the script to a variable
 version=$1
 # use sed to replace the image version in the file
-sed -i "s/landing-page.*/$version/g" .github/manifests/portfolio-app.yml
+sed -i "s/landing-page.*/landing-page:$version/g" .github/manifests/portfolio-app.yml
