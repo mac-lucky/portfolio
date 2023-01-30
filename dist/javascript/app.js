7c9123ea512690442cf6bbc40b17b8f3faeae0ab
Maciej Kędziora <maciejkedziora98@gmail.com>
2023-01-30 21:27:30 +0100

    make it compatible with safari

--- dist/javascript/app.js
+++ dist/javascript/app.js
@@ -24,3 +24,28 @@ document.querySelector("#Copy").onclick = function () {
 };
 
 
+const target = window.document.getElementById('neon');
+
+const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+const flickerAndColorText = text => 
+  text
+    .split('')
+    .map(flickerLetter)
+    .map(colorLetter)
+    .join('');
+const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
+
+
+neonGlory(target);
+target.onclick = ({ target }) =>  neonGlory(target);
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      neonGlory(entry.target);
+      observer.disconnect();
+    }
+  });
+});
+observer.observe(target);
