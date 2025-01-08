c47e26fa0b4f3dc9c82c0c07168d5cf38096d61b
Maciej Kędziora <44085390+mac-lucky@users.noreply.github.com>
2025-01-08 16:04:01 +0100

    Add version check in deployment script, update Dockerfile to use nginx:alpine, and enhance GitHub Actions workflow permissions

--- .github/scripts/deployment.sh
+++ .github/scripts/deployment.sh
@@ -2,4 +2,9 @@
 # assign the first argument passed to the script to a variable
 version=$1
 
+if [ -z "$version" ]; then
+  echo "Error: version is missing"
+  exit 1
+fi
+
 sed -i "s/landing-page.*/landing-page:$version/g" .github/manifests/portfolio-app.yml
